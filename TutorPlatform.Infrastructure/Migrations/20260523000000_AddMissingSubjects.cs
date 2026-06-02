c880fd614e3ac26bc5d5f5172cf88add5bbfdb37
huanti502 <minhhuan2112@gmail.com>
2026-06-02 11:13:39 +0700

    Suppress PendingModelChanges warning

---
+++ TutorPlatform.Infrastructure/Migrations/20260523000000_AddMissingSubjects.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using TutorPlatform.Infrastructure.Data;
+#nullable disable
+
+namespace TutorPlatform.Infrastructure.Migrations
+{
+    public partial class AddMissingSubjects : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Thêm 5 môn học còn thiếu (Id 6-10) vào DB hiện có
+            migrationBuilder.InsertData(
+                table: "Subjects",
+                columns: new[] { "Id", "Name", "Level", "IsActive" },
+                values: new object[,]
+                {
+                    { 6,  "Ngữ văn",      "THPT",       true },
+                    { 7,  "Lịch sử",      "THPT",       true },
+                    { 8,  "Tiếng Nhật",   "Đại học",    true },
+                    { 9,  "Toán cao cấp", "Đại học",    true },
+                    { 10, "IELTS",        "Chứng chỉ",  true }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(table: "Subjects", keyColumn: "Id", keyValues: new object[] { 6, 7, 8, 9, 10 });
+        }
+    }
+}
